fb44ef0b889c894a238b76046590375914e5cb86
U-VIASAT\galbacarys <galbacarys@LP-GALBACA-W764.hq.corp.viasat.com>
2016-02-24 08:15:20 -0800

    Added hooks for a couple of basic editing minor modes

--- init.el
+++ init.el
@@ -1,25 +1,41 @@
+;; UI
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; Packaging
 (require 'package)
 
-;Set a list of packages to install
+;; Set a list of packages to install
 (setq package-list '(evil paredit evil-paredit rainbow-delimiters))
 
-; Setting up the package archives
+;; Setting up the package archives
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
-; initialize packages
+;; initialize packages
 (package-initialize)
 
-; If we don't have a package cache, refresh it
+;; If we don't have a package cache, refresh it
 (package-refresh-contents)
 
-; make sure all packages are installed properly
+;; make sure all packages are installed properly
 (mapc
  (lambda (package)
    (or (package-installed-p package)
        (package-install package)))
  package-list)
 
-; now time to set up all the things!
+;; Package setups
+;; Evil: enabled everywhere, all the time, no exceptions.
 (evil-mode 1)
-(paredit-mode 1)
+
+;; Rainbow delimiters: enabled everywhere, all the time, no exceptions.
+;; for some reason, this requires a prog-mode hook rather than just a
+;; global declaration
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; Paredit: add a hook to elisp buffers
+;; I would love to enable this globally, but it doesn't make sense in
+;; certain contexts, which is really too bad.
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
 
